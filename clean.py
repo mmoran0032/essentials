08a504656fa4d4111c818f380f07a664f5ae8af5
Mike Moran <mmoran0032@gmail.com>
2016-01-19 15:33:26 -0500

    handling hidden files/directories

--- clean.py
+++ clean.py
@@ -9,6 +9,12 @@ extensions = ('aux', 'log', 'nav', 'out', 'snm', 'spl', 'syn', 'toc')
 directories = ('__pycache__', 'build', 'dist')
 
 
+def ignore_hidden(subdirs, files):
+    subdirs = [d for d in subdirs if not d.startswith('.')]
+    files = [f for f in files if not f.startswith('.')]
+    return subdirs, files
+
+
 def clean_files(directory, files):
     filtered = (f for f in files if f.split('.')[-1] in extensions)
     for f in filtered:
@@ -25,13 +31,14 @@ def clean_directories(directory, subdirs):
 
 def scrub_item(directory, item):
     name = '{}/{}'.format(directory, item)
-    _ = subprocess.call(['echo', 'Deleting: {}'.format(name)])
-    _ = subprocess.call(['rm', '-rf', '{}'.format(name)])
+    subprocess.call(['echo', 'Deleting: {}'.format(name)])
+    subprocess.call(['rm', '-rf', '{}'.format(name)])
 
 
 if __name__ == "__main__":
     start = '.'
     for directory, subdirs, files in os.walk(start):
+        subdirs[:], files = ignore_hidden(subdirs, files)
         clean_files(directory, files)
-        subdirs = clean_directories(directory, subdirs)
+        subdirs[:] = clean_directories(directory, subdirs)
 
