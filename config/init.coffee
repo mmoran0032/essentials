c8683a35bd636ff1956ea15c7b437d639acc5ef1
Mike Moran <mmoran0032@gmail.com>
2016-08-15 00:47:47 -0400

    Atom's `init.coffee` included

---
+++ config/init.coffee
@@ -0,0 +1,11 @@
+# Edits update the path so that python is found when not launching from bash
+
+# To have a dialogue window showing your path
+# alert(process.env.PATH)
+process.env.PATH = [
+    '/opt/miniconda3/bin',
+    process.env.PATH,
+    '/home/mikemoran/.local/bin'
+].join(':')
+
+# alert(process.env.PATH)
--- config/setup.py
+++ config/setup.py
@@ -10,11 +10,15 @@ home = '/home/mikemoran/'
 
 def main():
     files = os.listdir()
+    atom = 'init.coffee'
     files.remove('setup.py')
+    files.remove(atom)
     for f in files:
-        subprocess.call(
-            ['ln', '-s', '{}{}'.format(config, f), '{}.{}'.format(home, f)])
-
+        subprocess.run('ln -s {1}{0} {2}.{0}'.format(f, config, home),
+                       shell=True)
+    # special handling for Atom
+    subprocess.run('ln -s {1}{0} {2}.atom/{0}'.format(atom, config, home),
+                   shell=True)
 
 if __name__ == '__main__':
     main()
