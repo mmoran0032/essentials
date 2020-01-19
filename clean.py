de2566daf2d90468b56bb0e5c68f797aac4cb75e
Mike Moran <mmoran0032@gmail.com>
2020-01-19 12:47:59 -0500

    VSCodium + black

--- clean.py
+++ clean.py
@@ -6,14 +6,27 @@ from pathlib import Path
 import subprocess
 
 extensions = (
-    'aux', 'bbl', 'blg', 'fdb_latexmk', 'fls', 'lis', 'lof', 'log',
-    'lot', 'nav', 'out', 'snm', 'spl', 'syn', 'toc'
+    "aux",
+    "bbl",
+    "blg",
+    "fdb_latexmk",
+    "fls",
+    "lis",
+    "lof",
+    "log",
+    "lot",
+    "nav",
+    "out",
+    "snm",
+    "spl",
+    "syn",
+    "toc",
 )
-directories = ('__pycache__', 'build', 'dist')
-special_dirs = ('egg-info', 'ipynb_checkpoints', 'cache')
+directories = ("__pycache__", "build", "dist")
+special_dirs = ("egg-info", "ipynb_checkpoints", "cache")
 
 
-def main(start='.'):
+def main(start="."):
     filecount = 0
     dircount = 0
     for directory, subdirs, files in os.walk(start):
@@ -21,19 +34,19 @@ def main(start='.'):
         filecount += clean_files(directory, files)
         subdirs[:], number = clean_directories(directory, subdirs)
         dircount += number
-    print('== Files deleted: {}'.format(filecount))
-    print('== Directories deleted: {}'.format(dircount))
+    print("== Files deleted: {}".format(filecount))
+    print("== Directories deleted: {}".format(dircount))
 
 
 def ignore_hidden(subdirs, files):
-    subdirs_new = [d for d in subdirs if not d.startswith('.')]
-    subdirs_new.extend(d for d in subdirs if d.split('.')[-1] in special_dirs)
-    files = [f for f in files if not f.startswith('.')]
+    subdirs_new = [d for d in subdirs if not d.startswith(".")]
+    subdirs_new.extend(d for d in subdirs if d.split(".")[-1] in special_dirs)
+    files = [f for f in files if not f.startswith(".")]
     return subdirs_new, files
 
 
 def clean_files(directory, files):
-    filtered = [f for f in files if f.split('.')[-1] in extensions]
+    filtered = [f for f in files if f.split(".")[-1] in extensions]
     for f in filtered:
         scrub_item(directory, f)
     return len(filtered)
@@ -41,7 +54,7 @@ def clean_files(directory, files):
 
 def clean_directories(directory, subdirs):
     filtered = [d for d in subdirs if d in directories]
-    filtered.extend(d for d in subdirs if d.split('.')[-1] in special_dirs)
+    filtered.extend(d for d in subdirs if d.split(".")[-1] in special_dirs)
     for d in filtered:
         scrub_item(directory, d)
         subdirs.remove(d)
@@ -50,13 +63,13 @@ def clean_directories(directory, subdirs):
 
 def scrub_item(directory, item):
     name = Path(directory) / item
-    print(f'Deleting: {name}')
+    print(f"Deleting: {name}")
     # subprocess.run('rm -rf {}'.format(name), shell=True)
     if name.is_file():
         name.unlink()
     else:
-        subprocess.run('rm -rf {}'.format(str(name)), shell=True)
+        subprocess.run("rm -rf {}".format(str(name)), shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 
 root = Path.home()
-config = Path('config').resolve()
+config = Path("config").resolve()
 
 
 def main():
@@ -12,28 +12,28 @@ def main():
     for f in filepaths:
         target_path = determine_target_path(f)
         remove_and_link(f, target_path)
-    print('done')
+    print("done")
 
 
 def determine_target_path(f):
     # handle non-root install locations for particular files
-    if f.name == 'matplotlibrc':
-        _path = root / '.config' / 'matplotlib' / f.name
-    elif f.name == 'vscode-settings.json':  # update for VSCode
-        _path = root / '.config' / 'Code' / 'User' / 'settings.json'
+    if f.name == "matplotlibrc":
+        _path = root / ".config" / "matplotlib" / f.name
+    elif f.name == "vscode-settings.json":  # update for VSCode
+        _path = root / ".config" / "VSCodium" / "User" / "settings.json"
     else:
-        _path = root / f'.{f.name}'
+        _path = root / f".{f.name}"
     return _path
 
 
 def remove_and_link(orig, target):
     if target.exists():
-        print(f'unlinking {target}')
+        print(f"unlinking {target}")
         target.unlink()
     target.parent.mkdir(exist_ok=True)  # ensure folders are in place
-    print(f'linking {target.name}')
+    print(f"linking {target.name}")
     target.symlink_to(orig)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
